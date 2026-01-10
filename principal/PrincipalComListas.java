9934fac1c3ec70f51dbb412027bc8834144a1c16
MatheusPasqualoto <matheusjpasqualoto@gmail.com>
2026-01-10 16:38:37 -0300

    Segundo commit

--- principal/PrincipalComListas.java
+++ principal/PrincipalComListas.java
@@ -5,6 +5,9 @@ import modelos.Serie;
 import modelos.Titulo;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 public class PrincipalComListas {
     public static void main(String[] args) {
@@ -18,17 +21,33 @@ public class PrincipalComListas {
 
         Filme f1 = filmeDoPaulo;
 
-        ArrayList<Titulo> lista = new ArrayList<>();
-        lista.add(filmeDoPaulo);
-        lista.add(meuFilme);
-        lista.add(outroFilme);
-        lista.add(lost);
-        for (Titulo item: lista) {
+        List<Titulo> listaFilmes = new ArrayList<>();
+        listaFilmes.add(filmeDoPaulo);
+        listaFilmes.add(meuFilme);
+        listaFilmes.add(outroFilme);
+        listaFilmes.add(lost);
+        for (Titulo item: listaFilmes) {
             System.out.println(item.getNome());
             if (item instanceof  Filme filme && filme.getClassificacao() > 2) {
                 System.out.println("Classificação " + filme.getClassificacao());
             }
 
         }
+
+        ArrayList<String> buscaPorArtista = new ArrayList<>();
+        buscaPorArtista.add("ArtistaC");
+        buscaPorArtista.add("ArtistaB");
+        buscaPorArtista.add("ArtistaA");
+        System.out.println(buscaPorArtista);
+
+        Collections.sort(buscaPorArtista);
+        System.out.println(buscaPorArtista);
+
+        Collections.sort(listaFilmes);
+        System.out.println(listaFilmes);
+        listaFilmes.sort(Comparator.comparing(Titulo::getAnoDeLancamento));
+        System.out.println(listaFilmes);
+
+
     }
 }
