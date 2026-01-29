46a6c5fd13df811a62ea3cb39bb986a974900a25
MatheusPasqualoto <matheusjpasqualoto@gmail.com>
2026-01-29 15:08:53 -0300

    Adição de Try catch

--- src/principal/PrincipalComBuscas.java
+++ src/principal/PrincipalComBuscas.java
@@ -22,31 +22,38 @@ public class PrincipalComBuscas {
         var busca = leitura.nextLine();
 
         String endereco = "http://www.omdbapi.com/?t=" + busca + "&apikey=8731b53c";
-
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(endereco))
-                .build();
-        HttpResponse<String> response = client
-                .send(request, HttpResponse.BodyHandlers.ofString());
-
-        String json = response.body();
-        System.out.println(json);
-
-        Gson gson = new GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE).create();
-        //Titulo novoTitulo = gson.fromJson(json, Titulo.class);
-        TituloOMDB tituloOMDB = gson.fromJson(json, TituloOMDB.class);
-        System.out.println(tituloOMDB);
-
         try{
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(endereco))
+                    .build();
+            HttpResponse<String> response = client
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+
+            String json = response.body();
+            System.out.println(json);
+
+            Gson gson = new GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE).create();
+            //Titulo novoTitulo = gson.fromJson(json, Titulo.class);
+            TituloOMDB tituloOMDB = gson.fromJson(json, TituloOMDB.class);
+            System.out.println(tituloOMDB);
+
             Titulo testeTitulo = new Titulo(tituloOMDB);
+            System.out.println("********************Titulo convertido********************");
+            System.out.println("");
+            System.out.println(testeTitulo);
+            System.out.println("");
         } catch (NumberFormatException e){
             System.out.println("Aconteceu um erro: ");
             System.out.println(e.getMessage());
+        } catch(IllegalArgumentException e){
+            System.out.println("Verifique o nome pesquisado: ");
+            System.out.println(e.getMessage());
+        } catch(Exception e){
+            System.out.println("Erro não identificado");
         }
-        System.out.println("********************Titulo convertido********************");
-        System.out.println("");
-        System.out.println(testeTitulo);
-        System.out.println("");
+
+        System.out.println("Programa finalizado corretamente!");
+
     }
 }
